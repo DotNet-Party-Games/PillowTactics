b0e5263ee69ba4b5d8b85652a498b6ce045dcf9b
Joshua Dunkerson <jdunkerson@hotmail.com>
2021-07-30 14:46:39 -0400

    Changes added.

--- PillowFight.App/Controllers/HomeController.cs
+++ PillowFight.App/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 
 namespace PillowFight.App.Controllers
 {
-    [Authorize]
+    /*[Authorize]*/
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
