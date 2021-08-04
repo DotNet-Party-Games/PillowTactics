e7cb44c724bc9ed62e950a700f3f9f981cc969af
ieolivares <ieolivares9742@gmail.com>
2021-08-04 16:06:13 -0500

    Added Modules

--- PillowFightAngular/src/app/app.module.ts
+++ PillowFightAngular/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreatecharacterComponent } from './createcharacter/createcharacter.component';
+import { EditcharacterComponent } from './editcharacter/editcharacter.component';
+import { ArenaComponent } from './arena/arena.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    CreatecharacterComponent,
+    EditcharacterComponent,
+    ArenaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "profile", component: ProfileComponent},
+      {path: "create", component: CreatecharacterComponent},
+      {path: "edit", component: EditcharacterComponent},
+      {path: "arena", component: ArenaComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
