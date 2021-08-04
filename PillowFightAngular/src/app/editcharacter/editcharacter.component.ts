eb3836c5aa364e741fa7a71411a8fb7631d9ca5b
ieolivares <ieolivares9742@gmail.com>
2021-08-04 16:04:44 -0500

    Placeholder Edit Character Menu

---
+++ PillowFightAngular/src/app/editcharacter/editcharacter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editcharacter',
+  templateUrl: './editcharacter.component.html',
+  styleUrls: ['./editcharacter.component.css']
+})
+export class EditcharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
