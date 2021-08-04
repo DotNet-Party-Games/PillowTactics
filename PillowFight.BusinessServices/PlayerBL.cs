b48beb73a326d08335ae9d4a5e04f08b6d92fc28
mstringer2017 <matthewstringer@yahoo.com>
2021-08-04 14:31:29 -0400

    Added BL and DL logic for Player

---
+++ PillowFight.BusinessServices/PlayerBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PillowFight.Repositories.DataServices;
+using PillowFight.Repositories.Models;
+
+namespace PillowFight.BusinessServices
+{
+    class PlayerBL
+    {
+        private IDatastore _datastore;
+
+        public PlayerBL (IDatastore p_datastore)
+        {
+            _datastore = p_datastore;
+        }
+
+        public void CreatePlayer (Player p_player)
+        {
+            _datastore.CreatePlayer(p_player);
+        }
+
+        public Player GetPlayer(string p_username, string p_password)
+        {
+            return _datastore.GetPlayer(p_username, p_password);
+        }
+    }
+}
