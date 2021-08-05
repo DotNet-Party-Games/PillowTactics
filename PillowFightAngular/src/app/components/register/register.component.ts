2d0eaae1db032c54198e66bf6b802feab369e930
asapjules <asapjules1@gmail.com>
2021-08-05 14:38:19 -0400

    changed routing and added home page

--- PillowFightAngular/src/app/components/register/register.component.ts
+++ PillowFightAngular/src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, NgForm , FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, NgForm , FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
-
+import { IRegister } from 'src/app/shared/services/auth/Register';
 
 
 @Component({
@@ -10,24 +10,31 @@ import { AuthService } from 'src/app/shared/services/auth/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  //port: 31537?
+
     registration =  new FormGroup({
-    realName:new FormControl(),
-    email: new FormControl(),
-    username: new FormControl(),
-    password: new FormControl(),
-    confirmPassword: new FormControl(),
+    Email: new FormControl("", [Validators.required]),
+    Username: new FormControl("", [Validators.required]),
+    Password: new FormControl("", [Validators.required])
   });
   constructor(private authService: AuthService) {     
     
 }
   ngOnInit(): void {
   }
-  onSubmit(f:NgForm){
+  
+  registerPlayer(f:FormGroup)
+  {
     const registerObserver={
-      next:(x: any)=> console.log('User Created'),
+      next:(x: any)=> console.log('User logged in'),
       error:(err: any)=> console.log(err),
     }
-    this.authService.register(f.value).subscribe(registerObserver);
+    let tempPlayer:IRegister = {
 
+      Email: f.get("email")?.value,
+      Username:f.get("username")?.value,
+      Password:f.get("password")?.value
+    }
+    this.authService.register(tempPlayer).subscribe(registerObserver);
   }
 }
