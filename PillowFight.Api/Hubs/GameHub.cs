5a7ed2737f5d63f2981e456f1bd47cc583c3723b
Joshua Dunkerson <jdunkerson@hotmail.com>
2021-08-09 13:19:27 -0400

    Added changes.

--- PillowFight.Api/Hubs/GameHub.cs
+++ PillowFight.Api/Hubs/GameHub.cs
@@ -4,7 +4,6 @@ using PillowFight.Api.Models;
 using PillowFight.Repositories.Enumerations;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PillowFight.Api.Hubs
