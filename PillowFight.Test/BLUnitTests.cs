1a8e89e2aada4ed757c05aaf88a1a83d579c8dbf
mstringer2017 <matthewstringer@yahoo.com>
2021-08-11 11:49:02 -0400

    Testing gl.CreatePlayerAsync

--- PillowFight.Test/BLUnitTests.cs
+++ PillowFight.Test/BLUnitTests.cs
@@ -1,3 +1,4 @@
+using PillowFight.BusinessServices;
 using PillowFight.Repositories;
 using PillowFight.Repositories.DataServices;
 using PillowFight.Repositories.Enumerations;
@@ -17,7 +18,20 @@ namespace PillowFight.Test
         {
             _factory = new ConnectionFactory();
         }
-
+        [Theory]
+        [InlineData("foo@bar.baz", "Joe_Blow", "xyz")]
+        public void CreateOnePlayer(string p_email, string p_userName, string p_password)
+        {
+            PillowContext context = _factory.CreateContextForSQLite();
+            PostgresDatastore datastore = new(context);
+            PlayerBL bl = new(datastore);
+            bl.CreatePlayerAsync(p_userName, p_password, p_email).Wait();
+            Player newPlayer = context.Players.FirstOrDefault();
+            Assert.Equal(1, context.Players.Count()); // There is one row in the table
+            Assert.Equal(p_email, newPlayer.Email); //The fields match
+            Assert.Equal(p_userName, newPlayer.UserName);
+            Assert.Equal(p_password, newPlayer.Password);
+        }
 
     }
 }
